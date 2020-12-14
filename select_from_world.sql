4a768f3fe0abe0628e74c46368a749d15e505d53
uchennaanya <uchennaongit@gmail.com>
2020-12-14 15:53:04 +0100

    Add all result for select_from_world.sql

---
+++ select_from_world.sql
@@ -0,0 +1,52 @@
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
+SELECT name FROM world
+  WHERE population = 200000000
+
+SELECT name, population/1000000 FROM world 
+  WHERE continent = 'South America'
+
+SELECT name, population FROM world 
+  WHERE name IN ('France', 'Germany', 'Italy')
+
+SELECT name, population FROM world 
+  WHERE name IN ('France', 'Germany', 'Italy')
+
+SELECT name FROM world 
+  WHERE name LIKE '%United%'
+
+SELECT name, population, area FROM world 
+  WHERE area > 3000000 or population > 250000000
+
+SELECT name, population, area FROM world 
+  WHERE area > 3000000 XOR population > 250000000
+
+SELECT name, ROUND(population/1000000,2),
+             ROUND(gdp/1000000000,2) FROM world 
+  WHERE continent='South America'
+
+select name, ROUND(gdp/population,-3)
+from world
+where
+gdp>1000000000000
+
+SELECT name,      LENGTH(6),
+       continent, LENGTH(continent),
+       capital, LENGTH(6)
+  FROM world
+ WHERE name LIKE 'G%'
+
+SELECT name,capital
+FROM world
+WHERE LEFT(name,1)=LEFT(capital,1)
+AND name<>capital
+
+SELECT name
+  FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %'
